c9290432625a3a3b72661e1ebdb4e0ae52536cd6
ndicz <hnrmanesi@gmail.com>
2023-03-07 11:15:37 +0700

    Add project files.

---
+++ BlazorWepApp/Pages/Counter.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorWepApp.Pages
+{
+    public partial class Counter
+    {
+    
+    private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        private void Decrement()
+        {
+            if (currentCount > 0)
+            {
+                currentCount--;
+
+            }
+
+        }
+    }
+
+}
+
